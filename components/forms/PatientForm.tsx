90d262fd641703f1d925514b0f1c09826c34902c
MLarbo37 <mlarbo37@gmail.com>
2025-01-23 10:17:53 +0000

    setting up appwrite

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -8,6 +8,7 @@ import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -20,6 +21,7 @@ export enum FormFieldType {
 }
 
 const PatientForm = () => {
+    const router = useRouter()
     const [isLoading, setisLoading] = useState(false)
 
     const form = useForm<z.infer<typeof UserFormValidation>>({
@@ -31,8 +33,19 @@ const PatientForm = () => {
         },
     })
 
-    function onSubmit(values: z.infer<typeof UserFormValidation>) {
-        console.log(values)
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setisLoading(true);
+
+        try {
+            // const userData = {name, email, phone };
+
+            // const user = await createUser(userData)
+
+            // if(user) router.push(`/patients/${user.$id}/register`)
+        } catch (error) {
+            console.log(error)
+        }
+        setisLoading(false)
     }
     return (
         <Form {...form}>
